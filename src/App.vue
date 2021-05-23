456561a30bbcc444e5fcdadf267211d69f0d34d7
wisestme01 <c.nwagwu@genesystechhub.com>
2021-05-23 17:50:23 +0100

    filter completed tasks

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,27 @@
         </div>
       </div>
     </div>
+    <h4 class="bg-primary text-white text-center p-2">Incomplete Tasks</h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Task</div>
+        <div class="col-2 font-weight-bold">Done</div>
+      </div>
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+        <div class="col">{{t.action}}</div>
+        <div class="col-2 text-center">
+          <input type="checkbox" v-model="t.done" class="form-check-input">
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+          <label class="form-check-label font-weight-bold">
+            Hide completed tasks
+          </label>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,7 +51,13 @@ export default {
         {action: "Get shoes", done: false},
         {action: "Collect tickets", done: true},
         {action: "Call Ralph", done: false}
-      ]
+      ],
+      hideCompleted: true,
+    }
+  },
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks
     }
   }
 }
